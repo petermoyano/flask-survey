9f829ef587640a53ea803ebdb878aa34691106da
Pedro Moyano <pedromoyano454@gmail.com>
2021-09-18 11:58:21 -0300

    Step 8 completed with two different paths

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
 from surveys import satisfaction_survey
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 
 quiz = satisfaction_survey
-responses = []
 quest_replied = []
 num_questions = len(quiz.questions)
 count = f"/questions/{len(quest_replied)}"
+
 
 @app.route('/')
 def main():
     """Welcome page"""
     return render_template("base.html", quiz=quiz, quest_replied=quest_replied)
 
-@app.route('/questions/0')
+
+@app.route('/questions/0', methods=["POST"])
 def question():
+    session["responses"] = []
     if len(quest_replied) == 0:
         return render_template("questions/0.html", quiz=quiz)
     else:
         flash("You have been re-directed!")
         return redirect(f"/questions/{len(quest_replied)}")
+
 
 @app.route('/questions/1')
 def question1():
@@ -32,6 +35,7 @@ def question1():
         flash("You have been re-directed!")
         return redirect(f"/questions/{len(quest_replied)}")
 
+
 @app.route('/questions/2')
 def question2():
     if len(quest_replied) == 2:
@@ -39,6 +43,7 @@ def question2():
     else:
         flash("You have been re-directed!")
         return redirect(f"/questions/{len(quest_replied)}")
+
 
 @app.route('/questions/3')
 def question3():
@@ -48,17 +53,23 @@ def question3():
         flash("You have been re-directed!")
         return redirect(f"/questions/{len(quest_replied)}")
 
+
 @app.route("/answer", methods=["POST"])
 def answers():
-    if (num_questions -1) > len(quest_replied):
+    if (num_questions - 1) > len(quest_replied):
         ans = request.form["choices"]
-        responses.append(ans)
-        quest_replied.append("replied")
+        quest_replied.append(ans)
         count = f"/questions/{len(quest_replied)}"
+        responses = session["responses"]
+        responses.append(ans)
+        session["responses"] = responses
         return redirect(count)
     else:
         ans = request.form["choices"]
+        quest_replied.append(ans)
+        responses = session["responses"]
         responses.append(ans)
-        quest_replied.append("replied")
-        return render_template("/thanks.html", res = responses)
-    
+        session["responses"] = responses
+        session["responses2"] = quest_replied
+        ckies = request.cookies.get("responses")
+        return render_template("/thanks.html", quest_replied=quest_replied, ckies=ckies)
